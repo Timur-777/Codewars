bf42ebc291143347bbbeb2dd3d768cb0561113ff
Timur-777 <lechi002@inbox.ru>
2022-06-24 12:41:28 +0300

    8kyu_makeNegative1

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+function makeNegative(num) {
+    let res;
+    if(num > 0){
+        res = num * -1
+    }else if(num <= 0){
+        res = num * 1}
+    return res
+}
+function makeNegative1(num){
+    return num > 0 ? num * -1:num * 1
+}
 
-
+console.log(makeNegative1(5));
